d294c5d996bca5f996927f890f04716a4552073d
alinurammar <aalinur@stanford.edu>
2024-02-27 20:27:51 -0500

    Update prompt engineering and add verification layer

--- app.py
+++ app.py
@@ -10,15 +10,37 @@ app = Flask(__name__)
 def home_view():
         return "<h1>Recipe Generator Backend</h1>"
 
+def process_request(request_json):
+    ingredients_list_string = request_json.get('ingredients', '')
+    ingredients_list = [ingredient.strip() for ingredient in ingredients_list_string.split(',')]
+
+    includePantry = request_json.get('includePantry', '')
+    strictlyIngredients = request_json.get('strictlyIngredients', '')
+
+    selectedFilters = request_json.get('selectedFilters', {})
+    
+    # Extract and concatenate contents of selected filters
+    meal_style = selectedFilters.get('Meal Style', [])
+    dietary_restrictions = selectedFilters.get('Dietary Restrictions', [])
+    meal_speed = selectedFilters.get('Meal Speed', [])
+
+    return {
+        'ingredients': ingredients_list,
+        'includePantry': includePantry,
+        'strictlyIngredients': strictlyIngredients,
+        'meal_style': meal_style,
+        'dietary_restrictions': dietary_restrictions,
+        'meal_speed': meal_speed
+    }
+
+
 # Post request 
 @app.route("/ingredients", methods=['POST'])
 @cross_origin(origin='*')
 def handle_ingredients():
     data = request.json
-    ingredients_list_string = data.get('ingredients', '')
-    filter_list_string = data.get('filters', '')
-    ingredients_list = [ingredient.strip() for ingredient in ingredients_list_string.split(',')]
-    response =  gpt_client.generate_recipes(ingredientsList=ingredients_list, filtersList=filter_list_string)
+    request_obj = process_request(data)
+    response =  gpt_client.generate_recipes(request_obj)
     return jsonify({'message': response})
 if __name__ == '__main__':
    app.run(debug=True)
